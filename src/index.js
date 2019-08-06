85799024a62418875aad57bb6506c44fb4506467
edevars <e.devars.dev@gmail.com>
2019-08-06 11:11:00 -0500

    :fire: Hot module reloading

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
-import './global.css';
-import App from './components/App';
+import "./global.css";
+import App from "./components/App";
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
-ReactDOM.render(<App />, container);
+const render = () => {
+  ReactDOM.render(<App />, container);
+};
+
+render()
+
+if(module.hot){
+    module.hot.accept('./components/App', () => {
+        render()
+    })
+}
